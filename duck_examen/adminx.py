f90456585614248c7630da2eb73859b21b386af3
paul guichon <paul.guichon@iedparis8.net>
2015-02-12 15:19:39 +0100

    mise à jour de la gestion des dom tom

--- duck_examen/adminx.py
+++ duck_examen/adminx.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from django.core.urlresolvers import reverse
 from django.utils.encoding import smart_unicode
 from django_apogee.models import Pays
 from duck_examen.models import EtapeExamen, RattachementCentreExamen, ExamCenter
@@ -16,16 +17,41 @@ class PaysFilter(RelatedFieldListFilter):
         self.lookup_choices = [(x.pk, x.lib_pay) for x in Pays.objects.filter(examcenter__isnull=False).order_by('lib_pay').distinct()]
         return super(PaysFilter, self).choices()
 
+
+class DomTomFilter(RelatedFieldListFilter):
+    def choices(self):
+        self.lookup_choices = [(x.pk, x.label) for x in ExamCenter.objects.filter(has_incorporation=False).order_by('label').distinct()]
+        return super(DomTomFilter, self).choices()
+
+
 class RattachementCentreExamenAdmin(object):
     model = RattachementCentreExamen
     style = 'table'
     extra = 2
     max_num = 2
 
+    @filter_hook
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        # If it uses an intermediary model that isn't auto created, don't show
+        # a field in admin.
+
+        if isinstance(db_field, models.ManyToManyField) and not db_field.rel.through._meta.auto_created:
+            return None
+        attrs = self.get_field_attrs(db_field, **kwargs)
+        if db_field.name == 'centre' and self.request.GET.get('incorporation', 0) == '1':
+            query = ExamCenter.objects.filter(has_incorporation=False)
+            for x in self.model_instance.rattachementcentreexamen_set.all():
+                query = query | ExamCenter.objects.filter(pk=x.centre_id)
+            return db_field.formfield(queryset=query, **dict(attrs, **kwargs))
+        return db_field.formfield( **dict(attrs, **kwargs))
+
+
 
 class EtapeExamenAdmin(object):
     inlines = [RattachementCentreExamenAdmin]
-    list_filter = [('rattachementcentreexamen__centre__country', PaysFilter), 'cod_etp']
+    list_filter = [('rattachementcentreexamen__centre__country', PaysFilter), 'cod_etp',
+                   ('rattachementcentreexamen__centre', DomTomFilter)]
+
     fields = [
         'get_nom', 'get_prenom',
         'get_cod_etu', 'get_adresse',
@@ -53,6 +79,14 @@ class EtapeExamenAdmin(object):
                     'force_encaissement'
                     , css_class="unsort no_title"), horizontal=True, span=12)
             ))
+
+
+    @filter_hook
+    def model_admin_url(self, name, *args, **kwargs):
+        extention = '?incorporation={}'.format(self.request.GET.get('incorporation', 0))
+        return reverse(
+            "%s:%s_%s_%s" % (self.admin_site.app_name, self.opts.app_label,
+            self.module_name, name), args=args, kwargs=kwargs) + extention
 
     def get_nom(self, obj):
         return obj.cod_ind.lib_nom_pat_ind
