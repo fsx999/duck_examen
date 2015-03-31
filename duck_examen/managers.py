2a602e01de19543dd86a1ad3d19cc0c45f219cd9
paul guichon <paul.guichon@iedparis8.net>
2015-03-31 15:25:25 +0200

    migration django

--- duck_examen/managers.py
+++ duck_examen/managers.py
@@ -5,10 +5,14 @@ from django.db import models
 class ExamenCenterManager(models.Manager):
 
     def get_by_cod_etp_by_session(self, cod_etp, session, type_examen):
-        return self.filter(rattachementcentreexamen__inscription__cod_etp=cod_etp,
+        if type_examen:
+            return self.filter(rattachementcentreexamen__inscription__cod_etp=cod_etp,
                            rattachementcentreexamen__session=session, rattachementcentreexamen__type_examen__name=type_examen).distinct()
+        else:
+            return self.filter(rattachementcentreexamen__inscription__cod_etp=cod_etp,
+                           rattachementcentreexamen__session=session).distinct()
 
-    def get_incorporation_by_cod_etp_by_session(self, cod_etp, session, type_examen):
+    def get_incorporation_by_cod_etp_by_session(self, cod_etp, session, type_examen=None):
         return self.get_by_cod_etp_by_session(cod_etp, session, type_examen).filter(has_incorporation=True).distinct()
 
     def get_autre_by_cod_etp_by_session(self, cod_etp, session, type_examen):
